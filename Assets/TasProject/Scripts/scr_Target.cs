b47c28b9c59f794fdae1a3f0b26065fbc2b86cbf
Sami <sa2020v@gre.ac.uk>
2022-03-25 13:45:39 +0000

    hyrgbte

--- Assets/TasProject/Scripts/scr_Target.cs
+++ Assets/TasProject/Scripts/scr_Target.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class scr_Target : MonoBehaviour { 
 
     public float health = 50f;
-    [SerializeField] public int damageAmount = 20;
-
+    
     public void takeDamage(float amount)
     {
         health -= amount;
@@ -22,25 +21,4 @@ public class scr_Target : MonoBehaviour {
     {
         Destroy(gameObject);
     }
-
-
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        scr_DieCharacter damagedPlayer = collision.gameObject.GetComponent<scr_DieCharacter>();
-
-        if (damagedPlayer != null)
-        {
-
-            damagedPlayer.DealDamage(damageAmount);
-
-        }
-
-        if (collision.gameObject.name == "Player")
-
-       print(collision.gameObject.name + "colliding with object");
-    }
-
 }
-
